d2f484d55feef087aae70c657a68927653cdc190
ShahrukhMushtaq <shahrukhmushtaq23@gmail.com>
2019-01-28 12:33:03 +0500

    routes file

---
+++ fyp-backend/routes/modules.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var signup = require('./signup');
+var login = require('./login');
+var profile = require('./profile')
+var auth = require('../middleware/auth')
+
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+});
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 20
+  }
+});
+
+router.post('/register', upload.single('avtar'), signup);
+router.post('/login', login)
+router.get('/my/profile', auth, profile)
+
+module.exports = router;
