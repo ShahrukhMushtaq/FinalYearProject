93d5f48864c4fb05bdbb2ac622834e18969fa1eb
ShahrukhMushtaq <shahrukhmushtaq23@gmail.com>
2019-01-28 13:19:03 +0500

    Interceptor created

---
+++ fyp-frontend/src/app/http-interceptor.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpResponse,
+  HttpHandler,
+  HttpEvent,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpInterceptorService implements HttpInterceptor {
+
+  constructor() { }
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token: string = localStorage.getItem('token');
+
+    if (token) {
+      request = request.clone({ headers: request.headers.set('x-access-token',token) });
+    }
+
+    if (!request.headers.has('Content-Type')) {
+      request = request.clone({ headers: request.headers.set('Content-Type', 'application/json') });
+    }
+
+    request = request.clone({ headers: request.headers.set('Accept', 'application/json') });
+
+    return next.handle(request).pipe(
+      map((event: HttpEvent<any>) => {
+        if (event instanceof HttpResponse) {
+          console.log('event--->>>', event);
+        }
+        return event;
+      }),
+      catchError((error: HttpErrorResponse) => {
+        let data = {};
+        data = {
+          reason: error && error.error.message ? error.error.message : '',
+          status: error.status
+        };
+        console.log(data)
+        // this.notify.show(error.error.message, { position: 'top', duration: '1000', type: 'error' })
+        return throwError(error);
+      }));
+  }
+}
