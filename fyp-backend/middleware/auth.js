3d43ffb1ea0426c6a0f677c3f5418d8d7cc36cc7
ShahrukhMushtaq <shahrukhmushtaq23@gmail.com>
2019-01-28 12:34:17 +0500

    created Auth middle ware

---
+++ fyp-backend/middleware/auth.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken')
+
+function auth(req, res, next) {
+    const token = req.header('x-auth-token');
+    if (!token) return res.status(401).send({ message: 'Access denied. No token provided', status: 401 })
+
+    try {
+        const decode = jwt.verify(token, 'privateKey')
+        req.user = decode   // req.user._id
+        next()
+    } catch (ex) {
+        res.status(400).send({ message: 'Invalid token', status: 400 })
+    }
+}
+module.exports = auth;
