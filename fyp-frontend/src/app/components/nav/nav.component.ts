62a1a0f8dd270f224dae8b3432ed5380c5db29ee
ShahrukhMushtaq <shahrukhmushtaq23@gmail.com>
2019-01-30 12:17:00 +0500

    logout button and function

--- fyp-frontend/src/app/components/nav/nav.component.ts
+++ fyp-frontend/src/app/components/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from "../../services/auth.service";
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -7,9 +9,14 @@ import { AuthService } from "../../services/auth.service";
 })
 export class NavComponent implements OnInit {
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  logout() {
+    localStorage.removeItem("token");
+    this.router.navigate(['login']);
   }
 
 }
