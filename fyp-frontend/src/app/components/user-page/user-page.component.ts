12ec21830e771c9aeda62b31f749dd3cc58d64f8
ShahrukhMushtaq <shahrukhmushtaq23@gmail.com>
2019-02-02 19:20:32 +0500

    changed layout and styles

--- fyp-frontend/src/app/components/user-page/user-page.component.ts
+++ fyp-frontend/src/app/components/user-page/user-page.component.ts
@@ -12,4 +12,12 @@ export class UserPageComponent implements OnInit {
   ngOnInit() {
   }
 
+  setClass(items) {
+    let ul = document.getElementById('uls').getElementsByTagName('a')
+    for (let i = 0; i < 6; i++) {
+      ul[i].setAttribute("class", "nav-link")
+    }
+    items.setAttribute("class", "active nav-link")
+  }
+
 }
