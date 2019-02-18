9c2acced061d14fd8bf25844075f4455d5b358a1
shahrukhmushtaq <shahrukhmushtaq23@gmail.com>
2019-02-18 18:15:36 +0500

    GET auction API's

--- fyp-backend/routes/auction/get-auction-all.js
+++ fyp-backend/routes/auction/get-auction-all.js
@@ -1,10 +1,10 @@
 const Auction = require('../../models/auction')
-
 module.exports = async (req, res) => {
-    await Auction.find({}, (err, auction) => {
-        if (err) {
-            return res.status(404).send({ message: "Not Found", status: 404, content: "Null" })
-        }
-        res.status(200).send({ message: "Successfully Get Auctions", status: 200, content: auction })
-    })
+    var auctions = await Auction.find().populate("item");
+    if (auctions.length) {
+        res.status(200).send({ message: "Active Auctions", status: 200, content: auctions });
+    }
+    else {
+        return res.status(404).send({ message: "Not Found", status: 404, content: "Null" })
+    }
 }
--- fyp-backend/routes/auction/get-auction.js
+++ fyp-backend/routes/auction/get-auction.js
@@ -1,10 +1,11 @@
 const Auction = require('../../models/auction')
 
 module.exports = async (req, res) => {
-    await Auction.find({ user: req.params.id }, (err, auction) => {
-        if (err) {
-            return res.status(404).send({ message: "Not Found", status: 404, content: "Null" })
-        }
-        res.status(200).send({ message: "Successfully Get Auctions", status: 200, content: auction })
-    })
+    let auction = await Auction.find({ user: req.params.id }).populate("item")
+    if (auction.length) {
+        res.status(200).send({ message: "Active Auctions", status: 200, content: auction });
+    }
+    else {
+        return res.status(404).send({ message: "Not Found", status: 404, content: "Null" })
+    }
 }
