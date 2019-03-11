09525a31d6b7191967bac5013cf6e402df149d4d
shahrukhmushtaq <shahrukhmushtaq23@gmail.com>
2019-03-11 19:55:40 +0500

    socket work

--- fyp-frontend/src/app/components/messenger/messenger.component.ts
+++ fyp-frontend/src/app/components/messenger/messenger.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatService } from "../../services/chat.service";
+import { AuthService } from "../../services/auth.service";
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-messenger',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./messenger.component.css']
 })
 export class MessengerComponent implements OnInit {
-
-  constructor() { }
+  errors;
+  flag = false;
+  onlineUsers = [];
+  constructor(private chat: ChatService, private auth: AuthService) {
+    this.chat.setUserName(auth.CurrentUser._id)
+  }
 
   ngOnInit() {
+    this.chat.reveiveUsers()
+      .subscribe(data => {
+        console.log(data)
+        if (data == true) {
+          this.errors = "UserName Already Exist"
+          this.flag = true;
+        }
+        else {
+          // for (let i = 0; i < data.length; i++) {
+          //   if (data[i] == this.username) {
+          //     data.splice(i, 1)
+          //   } else {
+          //     this.onlineUsers.push(data[i])
+          //   }
+          // }
+          this.onlineUsers = data;
+          this.flag = false;
+        }
+      })
   }
 
 }
--- fyp-frontend/src/app/services/chat.service.ts
+++ fyp-frontend/src/app/services/chat.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from "rxjs";
+import { map } from 'rxjs/operators';
+import * as io from 'socket.io-client'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
-
+  private socket = io('http://localhost:3000')
   constructor() { }
+  sendMessage(msg) {
+    this.socket.emit('send message', msg)
+  }
+
+  receiveMessage() {
+    let observable = new Observable<any>(observer => {
+      this.socket.on('new message', (data) => {
+        console.log(data)
+        observer.next(data)
+      });
+      return () => { this.socket.disconnect(); }
+    })
+    return observable;
+  }
+  setUserName(username) {
+    this.socket.emit('userName', username)
+  }
+  reveiveUsers() {
+    let observable = new Observable<any>(observer => {
+      this.socket.on('allUsers', (data) => {
+        console.log(data)
+        observer.next(data)
+      });
+      return () => { this.socket.disconnect(); }
+    })
+    return observable;
+  }
+
+  receiveOldMsg() {
+    let observable = new Observable<any>(observer => {
+      this.socket.on('old messages', (data) => {
+        observer.next(data)
+      });
+      return () => { this.socket.disconnect(); }
+    })
+    return observable;
+  }
 }
